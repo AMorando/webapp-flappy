52d0aab4c2608b6b0683fbde6bddea84a7ca8b91
V5784 (Course ID) <v5784@cam.ac.uk>
2015-08-12 11:46:47 +0100

    3rd Day

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, "")));
 
 app.get("/", function(request, response){
-    response.sendFile(path.join(__dirname, "pages/index.html"));
+    response.sendFile(path.join(__dirname, "pages/game.html"));
 });
 
 app.post('/score', function(request, response){
@@ -18,3 +18,11 @@ var server = app.listen(8080, function() {
 
     console.log("Bob's Flappy Bird listening at http://%s:%s", host, port);
 });
+
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static(path.join(__dirname, "")));
+app.use(bodyParser.urlencoded({extended:true}));
--- js/stars.js
+++ js/stars.js
@@ -13,48 +13,20 @@ var labelScore;
 var star;
 var rand;
 var sprite5;
-var velocitystar;
-var player;
-//jQuery("#greeting-form").on("submit", function(event_details) {
-    //var greeting = "Hello ";
-   // var name1 = jQuery("#fullName").val();
-   // var greeting_message = greeting + name;
-  //  $("#greeting-form").hide();
-  //  $("#greeting").append("<p>" + greeting_message + "</p>");
-   // event_details.preventDefault();
-//});
-
-jQuery("#greeting-form").on("submit", function(event_details) {
-    var greeting = "Hello ";
-    var name = jQuery("#email").val();
-    var name1 = jQuery("#fullName").val();
-    var greeting_message = greeting + name1 + name;
-    $("#greeting-form").hide();
-    $("#greeting").append("<p>" + greeting_message + "</p>");
-});
-
+var velocity;
 function preload() {
     game.load.image("raindrop", "../assets/raindrop.png");
     game.load.image("backgroundImg", "../assets/myAwesomeBackground.png");
     game.load.audio("score", "../assets/point.ogg");
     game.load.image("star", "../assets/star.png");
-    game.load.image("playerImg", "../assets/flappy.png");
 }
 function create() {
     game.stage.setBackgroundColor("#00FF00");  // set the background colour of the scene (#B2C2D2 was good)
     var background = game.add.image(0, 0, "backgroundImg");
     background.width = 790;
     background.height = 400;
-    game.add.text(160, 170, "Use arrows to move from side to side. Click to make raindrops :)",
-        {font: "17px Cooper Std Black", fill: "#00FF00"});
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    player = game.add.sprite(80, 200, "playerImg");
-    game.physics.arcade.enable(player);
-    //
-    player.body.velocity.y = -100;
-    player.body.gravity.y = 200;
-
-
+    game.add.text(160, 170, "Welcome to my game!",
+        {font: "40px Cooper Std Black", fill: "#00FF00"});
     var sprite1 = game.add.sprite(10, 350, "raindrop");
     sprite1.width = 20;
     sprite1.height = 30;
@@ -72,33 +44,19 @@ function create() {
         .add(clickHandler);
     game.input
         .keyboard.addKey(Phaser.Keyboard.SPACEBAR)
-        .onDown.add(playerJump);
-    game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
-        .onDown.add(moveRight);
-    game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
-        .onDown.add(moveLeft);
+        .onDown.add(spaceHandler);
     labelScore = game.add.text(50, 20, "0");
     star = game.add.sprite(0, 0, "star");
-    game.physics.arcade.enable(star);
     var rand = game.rnd.integerInRange(1 , 750);
     star.x = rand;
     star.y = 0;
-    velocitystar = 1;
+    velocity = 1
     //game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
         //.onDown.add(moveStarDown);
 
 }
 function moveStarDown(){
-    star.y = star.y + velocitystar
-}
-
-function moveRight(){
-    //player.x += 10;
-    player.body.velocity.x = 100;
-}
-function moveLeft(){
-    //player.x -= 10;
-    player.body.velocity.x = -100;
+    star.y = star.y + velocity
 }
 /*
  * Loads all resources for the game and gives them names.
@@ -114,11 +72,10 @@ function clickHandler(event){
     changeScore();
 }
 
-function playerJump() {
-    player.body.velocity.y = -200;
+
+function spaceHandler(){
     game.sound.play("score");
 }
-
 /*
  * This function updates the scene. It is called for every new frame.
  */
@@ -129,59 +86,18 @@ function update() {
   if(star.y >= 360){
       backToTop();
   }
-  if(player.y > 390){
-      gameOver();
-  }
-   if(star==null){console.log("star")};
-   // console.log(player);
-  game.physics.arcade.overlap(player, star, changeScore);
-
  // game.overlap(star, sprite5, changeScore);
 }
-function gameOver(){
-    game.destroy();
-    $("#score").val(score.toString());
-    $("#greeting").show();
-    setTimeout(formhide, 20000);
-}
-
-
-function formhide(){
-    $("#greeting").hide();
-}
-
 function backToTop(){
     var rand = game.rnd.integerInRange(1 , 750);
     star.x = rand;
     star.y = 0;
-    if(velocitystar <= 10){
-        velocitystar = velocitystar + 1;
+    if(velocity <= 20){
+        velocity = velocity + 1;
     }
     moveStarDown();
 }
-
-function generateStar(){
-    star = game.add.sprite(0, 0, "star");
-    var rand = game.rnd.integerInRange(1 , 750);
-    star.x = rand;
-    star.y = 0;
-    velocitystar = 1;
-}
-
-
-
 function changeScore(){
-    score ++;
+   score = score + 1;
     labelScore.setText(score.toString());
-
-    var rand = game.rnd.integerInRange(1 , 750);
-    star.x = rand;
-    star.y = 0;
-    velocitystar = 1;
 }
-
-
-//function game_over(){
-   // player.kill();
-   // $("#score").val(score.toString());
-//}
